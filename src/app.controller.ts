2651ce315cd45a543df7ad9aa806809c027269a4
신종민 <ace4463@gmail.com>
2024-03-31 19:48:53 +0900

    feat: redis 설정 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
+import Redis from 'ioredis';
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { InjectRedis } from '@nestjs-modules/ioredis';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @InjectRedis() private readonly redis: Redis,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello() {
+    await this.redis.set('key', 'Redis data!');
+    const redisData = await this.redis.get('key');
+    return { redisData };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { RedisModule } from '@nestjs-modules/ioredis';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    RedisModule.forRootAsync({
+      useFactory: () => ({
+        type: 'single',
+        url: 'redis://localhost:6379',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
